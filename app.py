a3cde369af5061d252c62422f84d26f86b53b3f8
gkjohns <gkjohns@gmail.com>
2016-10-31 13:40:09 -0400

    create hello world

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, render_template, flash,\
+                  session, redirect, url_for
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'comsw4111'
+app.debug = True
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run()
